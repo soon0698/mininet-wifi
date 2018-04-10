cfe5e34ddbba268609b40d71e1a7cf24a78e345d
Nick Gordon <nmgordon@memphis.edu>
2018-04-10 15:18:37 -0500

    Tweaked install file. Hope to find mininet dependencies now.

--- util/install.sh
+++ util/install.sh
@@ -154,9 +154,11 @@ function mn_deps {
 	    fi
     fi
     echo "Installing Mininet core"
-    git clone https://github.com/mininet/mininet mininet-base
-    pushd mininet-base
+    pushd $MININET_DIR
+    git clone --depth 1 https://github.com/mininet/mininet mininet-base
+    pushd $MININET_DIR/mininet-base
     sudo make install
+    popd
     popd
     echo "Installing Mininet-wifi core"
     pushd $MININET_DIR/mininet-wifi
